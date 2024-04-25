1aea7089b20e9eaec735687d59f4d2ced8979b12
Jennifer <imjennyyang@gmail.com>
2024-04-26 00:19:57 +0200

    feat: add delete page with associated styling and scripts

---
+++ delete.js
@@ -0,0 +1,9 @@
+document.getElementById('return').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = './finish.html';
+});
+
+/* document.getElementById('del').addEventListener('click', function(event) {
+    event.preventDefault();
+    // window.location.href = './del.html';
+}); */
